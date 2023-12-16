184c28b635ab084f9e519bc25fea2404f1dc897b
msmhsnl <msmhsnl@gmail.com>
2023-12-16 19:59:19 +0300

    ADD .env

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const testText = process.env.EXPO_PUBLIC_TEST;
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!123</Text>
+      <Text>Open up App.tsx to start working on your app!{testText}</Text>
       <StatusBar style="auto" />
     </View>
   );
