2e9a9d1bc2916c28c94fc61bf7a0a8be0f4b9e99
msmhsnl <msmhsnl@gmail.com>
2023-12-20 19:50:19 +0300

    UPDATE tabbar & CartCountBubble integration

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet } from "react-native";
+import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import HomeScreen from "./src/screens/HomeScreen";
 import DetailScreen from "./src/screens/DetailScreen";
@@ -11,6 +12,8 @@ import type { RootTabParamList } from "./src/navigation/NavigationTypes";
 import { Provider } from "react-redux";
 import store from "./src/redux/store";
 
+import CartCountBubble from "./src/components/CartCountBubble/CartCountBubble";
+
 const Tab = createBottomTabNavigator<RootTabParamList>();
 
 export default function App() {
@@ -18,17 +21,48 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarShowLabel: false,
+              tabBarIcon: ({ focused }) => {
+                return (
+                  <Ionicons
+                    name="home-sharp"
+                    size={24}
+                    color={focused ? "orange" : "gray"}
+                  />
+                );
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Cart"
+            component={CartScreen}
+            options={{
+              tabBarShowLabel: false,
+              tabBarIcon: ({ focused }) => {
+                return (
+                  <View className="relative">
+                    <CartCountBubble />
+                    <Ionicons
+                      name="cart"
+                      size={24}
+                      color={focused ? "orange" : "gray"}
+                    />
+                  </View>
+                );
+              },
+            }}
+          />
           <Tab.Screen
             name="Detail"
             component={DetailScreen}
             options={{ tabBarItemStyle: { display: "none" } }}
           />
-          <Tab.Screen name="Cart" component={CartScreen} />
         </Tab.Navigator>
       </NavigationContainer>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({});
